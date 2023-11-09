48a72503dcae6d6c590d4c00d5dffca365c3f835
Vasilyev, Artem <artem001v@gmail.com>
2023-11-09 18:28:36 +0300

    retrieve table names of current connetion

--- src/extension.ts
+++ src/extension.ts
@@ -1,45 +1,55 @@
-'use strict';
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
-import * as vscode from 'vscode';
+"use strict";
+import * as vscode from "vscode";
+import * as azdata from "azdata";
 
-// The module 'azdata' contains the Azure Data Studio extensibility API
-// This is a complementary set of APIs that add SQL / Data-specific functionality to the app
-// Import the module and reference it with the alias azdata in your code below
-
-import * as azdata from 'azdata';
-
-// this method is called when your extension is activated
-// your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+  let adsLog = vscode.window.createOutputChannel("ADS Search Tables");
+  adsLog.appendLine("ADS Search Tables Activated");
+  let tableNames: string[] = [];
 
-    // Use the console to output diagnostic information (console.log) and errors (console.error)
-    // This line of code will only be executed once when your extension is activated
-    console.log('Congratulations, your extension "ads-search-tables" is now active!');
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "ads-search-tables.helloWorld",
+      async () => {
+        const connection = await azdata.connection.getCurrentConnection();
+        const uri = await azdata.connection.getUriForConnection(
+          connection.connectionId
+        );
+        console.log(connection.databaseName);
+        const provider = azdata.dataprotocol.getProvidersByType(
+          azdata.DataProviderType.QueryProvider
+        )[0] as azdata.QueryProvider;
+        const results = await provider.runQueryAndReturn(
+          uri,
+          `SELECT CONCAT(s.name, '.', t.name) from sys.tables t
+           join sys.schemas s on s.schema_id = t.schema_id`
+        );
+        adsLog.appendLine(JSON.stringify("table names loaded"));
+        tableNames = results.rows.map((row) => row[0].displayValue);
+      }
+    )
+  );
 
-    // The command has been defined in the package.json file
-    // Now provide the implementation of the command with  registerCommand
-    // The commandId parameter must match the command field in package.json
-    context.subscriptions.push(vscode.commands.registerCommand('ads-search-tables.helloWorld', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        vscode.window.showInformationMessage('Hello World!');
-    }));
-
-    context.subscriptions.push(vscode.commands.registerCommand('ads-search-tables.showCurrentConnection', () => {
-        // The code you place here will be executed every time your command is executed
-
-        // Display a message box to the user
-        azdata.connection.getCurrentConnection().then(connection => {
-            let connectionId = connection ? connection.connectionId : 'No connection found!';
+  context.subscriptions.push(
+    vscode.commands.registerCommand(
+      "ads-search-tables.showCurrentConnection",
+      () => {
+        azdata.connection.getCurrentConnection().then(
+          (connection) => {
+            console.log(connection);
+            let connectionId = connection
+              ? connection.connectionId
+              : "No connection found!";
             vscode.window.showInformationMessage(connectionId);
-        }, error => {
-             console.info(error);
-        });
-    }));
+          },
+          (error) => {
+            console.info(error);
+          }
+        );
+      }
+    )
+  );
 }
 
 // this method is called when your extension is deactivated
-export function deactivate() {
-}
+export function deactivate() {}
